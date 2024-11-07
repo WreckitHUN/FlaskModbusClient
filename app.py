3238e6bd15ae034ac39b1332638974d1ffaba4dc
Tóth Szabolcs Balázs <szabolcs978@gmail.com>
2024-11-07 09:42:48 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, jsonify
+from pymodbus.client import ModbusTcpClient
+app = Flask(__name__)
+
+# Server's IP port is default 502
+local = '127.0.0.1'
+PLC = '192.168.0.1'
+ESP32 = "0.0.0.0"
+
+client = ModbusTcpClient(host=local)
+client.connect()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/input', methods=["POST"])
+def change_input():
+    if not request.method == "POST":
+        return
+    data = request.get_json()
+    """ {
+    address: 0,
+    value: 0
+    } """
+    client.write_coil(address=data["address"], value=data["value"])
+    return jsonify("OK")
+
+
+@app.route('/outputs', methods=["GET"])
+def read_outputs():
+    if not request.method == "GET":
+        return
+    outputs = client.read_discrete_inputs(address=0, count=8)
+    sent_data = jsonify(outputs.bits)
+    return sent_data
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
---
+++ static/main.js
@@ -0,0 +1,66 @@
+// Create inputs
+createInputs(8);
+// Create outputs
+createOutputs(8);
+// Read outputs at a set interval 50ms
+setInterval(() => {
+  readOutputs();
+}, 50);
+
+async function changeInput(input) {
+  const response = await fetch("/input", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(input),
+  });
+  const receivedData = await response.json();
+  console.log(receivedData);
+}
+
+async function readOutputs() {
+  const response = await fetch("/outputs");
+  const outputArray = await response.json(); // [false, false, false, ...]
+  outputArray.forEach((value, i) => {
+    // Find the corresponding output
+    const lampElement = document.querySelector(`#Q${i}`);
+    if (value) {
+      lampElement.classList.add("on");
+    } else {
+      lampElement.classList.remove("on");
+    }
+  });
+}
+
+function createInputs(number) {
+  const inputField = document.querySelector("#inputs");
+  // Using special for loop ;) for creating multiple buttons
+  [...Array(number)].forEach((item, i) => {
+    // Create button HTML element
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = `I${i}`;
+    buttonElement.id = `I${i}`;
+    buttonElement.classList.add("btn");
+    // Add event listeners to the buttons (down & up)
+    buttonElement.addEventListener("mousedown", () => {
+      changeInput({ address: i, value: 1 });
+    });
+    buttonElement.addEventListener("mouseup", () => {
+      changeInput({ address: i, value: 0 });
+    });
+    // Add the input to the inputField HTML div element
+    inputField.appendChild(buttonElement);
+  });
+}
+
+function createOutputs(number) {
+  const outputField = document.querySelector("#outputs");
+  // Using special for loop ;) for creating multiple outputs
+  [...Array(number)].forEach((item, i) => {
+    const lampElement = document.createElement("div");
+    lampElement.classList.add("lamp");
+    lampElement.id = `Q${i}`;
+    outputField.appendChild(lampElement);
+  });
+}
